ce5d0c9e258a8fd916bc7ebb292f24bd1b47f826
vladimer <gshatakishvili@gmail.com>
2022-06-10 23:52:01 +0400

    Rename name to type for table info

--- src/fields/TableField.js
+++ src/fields/TableField.js
@@ -10,7 +10,7 @@ export default class TableField {
 
     getFieldInfo() {
         return {
-            name: this.getFieldName(),
+            type: this.getFieldName(),
             params: this.getFieldParams(),
         }
     }
